f5fffa688f2e4ddec3ac805f94e332c69d6d67f9
Bella <bella.csouza5@gmail.com>
2024-06-08 14:08:02 -0300

    Inclusão de pacote log-symbols

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import chalk from "chalk";
+import logSymbols from "log-symbols";
 
 console.log(chalk.italic.bgRed.bold("Hello world!"));
+
+console.log(logSymbols.success, chalk.green.italic("Success!"));
